b8bb9f48f9c3ba84d700979fe9c8bb0e524b6fbd
Lenta-Maximiliano <150547622+Lenta-Maximiliano@users.noreply.github.com>
2024-10-02 23:08:48 -0300

    Crear vista de productos en ItemListContainer.jsx usando ProductsCard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ItemListContainer from './components/View/ItemListView/ItemListContainer'
+import './App.css'
 
 function App() {
-  
   return (
-    <>
-      <ItemListContainer />
-    </>
+    <BrowserRouter>
+      <div className='app-container'>
+      <main className='app-main'>
+            <Routes>
+              <Route path="/" element={<ItemListContainer />} />
+            </Routes>
+          </main>
+      </div>
+    </BrowserRouter>
+  
   )
 }
 
